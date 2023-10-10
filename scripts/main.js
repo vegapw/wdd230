8bac25b8bcf9d44f2252a176628482fe1ac93aea
frevega <frevega@publicisgroupe.net>
2023-10-10 12:03:04 -0500

    Home page enhanced with map and responsive menu.

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,11 @@ date1.textContent = date0.getFullYear();
 date2.textContent = date0.toLocaleDateString() + " - " + date0.toLocaleTimeString([], {
     hour12: false,
 });
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
+});
