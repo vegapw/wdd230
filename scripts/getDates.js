a39a913337e85abdf6600d91e4b1bf2c1246e07e
Fredy Vega <vegapw@gmail.com>
2024-06-27 16:03:30 -0600

    Updated Portal home page w02

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,3 +1,4 @@
+//Variables for home page dates
 const options = {
   year: "numeric"
 };
@@ -11,6 +12,25 @@ const options1 = {
 };
 const year = new Date().toLocaleDateString("en-US", options);
 const lastModified = new Date(document.lastModified).toLocaleDateString("en-US", options1);
+// Variables for hamburguer button
+const hamButton = document.querySelector('#menu');
+const navMenu = document.querySelector('.menuItems');
+// Variables for dark mode
+const darkButton = document.querySelector('#darkButton');
+const main = document.querySelector('main');
+const homeDiv = document.querySelector('.home');
 
 document.getElementById("year").textContent = year;
 document.getElementById("lastModified").textContent = lastModified;
+
+
+hamButton.addEventListener('click', () => {
+  navMenu.classList.toggle('open');
+  hamButton.classList.toggle('open');
+});
+
+
+darkButton.addEventListener('click', () => {
+  main.classList.toggle('dark');
+  homeDiv.classList.toggle('dark');
+});
