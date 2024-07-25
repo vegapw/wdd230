c97f52af1a157a667321ad894f57472fbf895c89
Fredy Vega <vegapw@gmail.com>
2024-07-25 16:11:00 -0600

    Chamber update w06, spotlight and Forecast updated

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -16,16 +16,15 @@ async function getCompanies() {
 }
 
 function displayMembers(members) {
-    const premiumMembers = members.filter((member) => member.membershiplevel === 'gold' || member.membershiplevel === 'silver');
+    let premiumMembers = members.filter((member) => member.membershiplevel === 'gold' || member.membershiplevel === 'silver');
     let showMembers = [];
     let index = 0;
     while (index < 3) {
-        if (!showMembers.includes(getRandom(premiumMembers))) {
-            showMembers.push(getRandom(premiumMembers));
-            index++;
-        }
+        let company = getRandom(premiumMembers);
+        premiumMembers = premiumMembers.filter((member) => member.phonenumber !== company.phonenumber);
+        showMembers.push(company);
+        index++;
     }
-    console.log(showMembers);
     showMembers.forEach(member => {
         const section = document.createElement('section');
         const title = document.createElement('h3');
@@ -33,17 +32,21 @@ function displayMembers(members) {
         const phone = document.createElement('p');
         const psite = document.createElement('p');
         const wsite = document.createElement('a');
+        const membership = document.createElement('p');
         section.setAttribute('class', `${member.membershiplevel}, card`);
         title.textContent = member.name;
         address.textContent = member.address;
         phone.textContent = member.phonenumber;
         wsite.setAttribute('href', member.website);
         wsite.textContent = member.website;
+        membership.setAttribute('class', 'membership');
+        membership.textContent = member.membershiplevel;
         psite.appendChild(wsite);
         section.appendChild(title);
         section.appendChild(address);
         section.appendChild(phone);
         section.appendChild(psite);
+        section.appendChild(membership);
         news.appendChild(section);
     });
 }
